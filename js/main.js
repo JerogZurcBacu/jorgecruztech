4fbdebaaec9fef83036d740536165b0497819fb7
Jorge Cruz <jorgecruzprof@gmail.com>
2023-11-23 17:00:23 -0600

    Actualización js - html
    carga de modificaciones de js y html para selector de idiomas

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const idiomas = document.getElementsByClassName('opcion');
 // Info
 const holaMundo = document.getElementById('hola-mundo');
 const miNombre = document.getElementById('mi-nombre');
+const yoSoy = document.getElementById('yo-soy');
+const misProyectos = document.getElementById('mis-proyectos');
+const sobreMi = document.getElementById('sobre-mi');
 
 //Toggle lista idiomas
 idiomaActual.addEventListener('click', ()=>{
@@ -29,13 +32,40 @@ function establecerIdioma(idioma) {
     switch (idioma) {
         case 'english':
             holaMundo.innerHTML = '<span id="negritas">¡HELLO</span> WORLD!';
-            miNombre.textContent = 'My names is Jorge Cruz';
+            miNombre.innerHTML = 'My name is <span id="negritas">Jorge Cruz</span>';
+            yoSoy.innerHTML = 'and I&#x27m <span id="negritas">Front-End Developer</span>';
+            misProyectos.textContent = 'My Proyects';
+            sobreMi.textContent = 'About Me';
             break;
         case 'portuguese':
-            holaMundo.textContent = 'Olá Mundo';
-            miNombre.textContent = 'Meu nome é Jorge Cruz';
+            holaMundo.innerHTML = '<span id="negritas">¡OLÁ</span> MUNDO!';
+            miNombre.innerHTML = 'Meu nome é <span id="negritas">Jorge Cruz</span>';
+            yoSoy.innerHTML = 'e sou <span id="negritas">Desenvolvedor Front-End</span>';
+            misProyectos.textContent = 'Meus Projetos';
+            sobreMi.textContent = 'Sobre mim';
+            break;
+        case 'español':
+            holaMundo.innerHTML = '<span id="negritas">¡HOLA</span> MUNDO!';
+            miNombre.innerHTML = 'Mi nombre es<span id="negritas">Jorge Cruz</span>';
+            yoSoy.innerHTML = 'y soy <span id="negritas">Desarrollador Front-End</span>';
+            misProyectos.textContent = 'Mis Proyectos';
+            sobreMi.textContent = 'Sobre mi';
             break;
         default:
             break;
     }
 }
+
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log(navigator.language);
+    switch (navigator.language) {
+        case 'en-US':
+            establecerIdioma('usa')
+            break;
+        case 'es-ES':
+            establecerIdioma('español')
+            break;
+        default:
+            break;
+    }
+});
